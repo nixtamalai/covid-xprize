f97acd77419ab6ecc813251266e40e0a22e89870
plablo09 <pablo.lopez@centrogeo.edu.mx>
2021-02-08 11:36:25 -0600

    New bootstrap design.

--- covid_xprize/nixtamalai/viz_components.py
+++ covid_xprize/nixtamalai/viz_components.py
@@ -1,5 +1,6 @@
 import pandas as pd
-import plotly.express as px
+import dash_html_components as html
+import dash_core_components as dcc
 from covid_xprize.scoring.prescriptor_scoring import compute_pareto_set
 from covid_xprize.nixtamalai.prescriptors import get_greedy_prescription_df
 from covid_xprize.nixtamalai.prescriptors import generate_cases_and_stringency_for_prescriptions
@@ -79,3 +80,210 @@ def get_overall_data(start_date, end_date, ip_file, weights_df, model):
                                                             weights_df)
     overall_pdf = df.groupby('PrescriptionIndex').mean().reset_index()
     return overall_pdf, predictions
+
+def get_sliders(BASE_COSTS):
+    sliders = [
+        html.Div([html.P(children=dcc.Markdown("School closing (**C1**)")),
+        dcc.Slider(
+            id='C1-weight',
+            min=0,
+            max=1,
+            step=0.1,
+            value=BASE_COSTS['C1_School closing'],
+            marks={0: "0", 0.5: "0.5", 1: "1"},
+            tooltip={'always_visible': False}
+        )]),
+        html.Div([html.P(children=dcc.Markdown("Workplace closing (**C2**)")),
+        dcc.Slider(
+            id='C2-weight',
+            min=0,
+            max=1,
+            step=0.1,
+            value=BASE_COSTS['C2_Workplace closing'],
+            marks={0: "0", 0.5: "0.5", 1: "1"},
+            tooltip={'always_visible': False}
+
+        )]),
+        html.Div([html.P(children=dcc.Markdown("Cancel public events (**C3**)")),
+        dcc.Slider(
+            id='C3-weight',
+            min=0,
+            max=1,
+            step=0.1,
+            value=BASE_COSTS['C3_Cancel public events'],
+            marks={0: "0", 0.5: "0.5", 1: "1"},
+            tooltip={'always_visible': False}
+
+        )]),
+        html.Div([html.P(children=dcc.Markdown("Restrictions on gathering (**C4**)")),
+        dcc.Slider(
+            id='C4-weight',
+            min=0,
+            max=1,
+            step=0.1,
+            value=BASE_COSTS['C4_Restrictions on gatherings'],
+            marks={0: "0", 0.5: "0.5", 1: "1"},
+            tooltip={'always_visible': False}
+
+        )]),
+        html.Div([html.P(children=dcc.Markdown("Close public transport (**C5**)")),
+        dcc.Slider(
+            id='C5-weight',
+            min=0,
+            max=1,
+            step=0.1,
+            value=BASE_COSTS['C5_Close public transport'],
+            marks={0: "0", 0.5: "0.5", 1: "1"},
+            tooltip={'always_visible': False}
+
+        )]),
+        html.Div([html.P(children=dcc.Markdown("Stay at home requirements (**C6**)")),
+        dcc.Slider(
+            id='C6-weight',
+            min=0,
+            max=1,
+            step=0.1,
+            value=BASE_COSTS['C6_Stay at home requirements'],
+            marks={0: "0", 0.5: "0.5", 1: "1"},
+            tooltip={'always_visible': False}
+
+        )]),
+        html.Div([html.P(children=dcc.Markdown(
+            "Restrictions on internal movement (**C7**)")),
+        dcc.Slider(
+            id='C7-weight',
+            min=0,
+            max=1,
+            step=0.1,
+            value=BASE_COSTS['C7_Restrictions on internal movement'],
+            marks={0: "0", 0.5: "0.5", 1: "1"},
+            tooltip={'always_visible': False}
+
+        )]),
+        html.Div([html.P(children=dcc.Markdown("International travel controls (**C8**)")),
+        dcc.Slider(
+            id='C8-weight',
+            min=0,
+            max=1,
+            step=0.1,
+            value=BASE_COSTS['C8_International travel controls'],
+            marks={0: "0", 0.5: "0.5", 1: "1"},
+            tooltip={'always_visible': False}
+
+        )]),
+        html.Div([html.P(children=dcc.Markdown("Public information campaigns (**H1**)")),
+        dcc.Slider(
+            id='H1-weight',
+            min=0,
+            max=1,
+            step=0.1,
+            value=BASE_COSTS['H1_Public information campaigns'],
+            marks={0: "0", 0.5: "0.5", 1: "1"},
+            tooltip={'always_visible': False}
+
+        )]),
+        html.Div([html.P(children=dcc.Markdown("Testing policy (**H2**)")),
+        dcc.Slider(
+            id='H2-weight',
+            min=0,
+            max=1,
+            step=0.1,
+            value=BASE_COSTS['H2_Testing policy'],
+            marks={0: "0", 0.5: "0.5", 1: "1"},
+            tooltip={'always_visible': False}
+
+        )]),
+        html.Div([html.P(children=dcc.Markdown("Contact tracing (**H3**)")),
+        dcc.Slider(
+            id='H3-weight',
+            min=0,
+            max=1,
+            step=0.1,
+            value=BASE_COSTS['H3_Contact tracing'],
+            marks={0: "0", 0.5: "0.5", 1: "1"},
+            tooltip={'always_visible': False}
+
+        )]),
+        html.Div([html.P(children=dcc.Markdown("Facial coverings (**H4**)")),
+        dcc.Slider(
+            id='H4-weight',
+            min=0,
+            max=1,
+            step=0.1,
+            value=BASE_COSTS['H6_Facial Coverings'],
+            marks={0: "0", 0.5: "0.5", 1: "1"},
+            tooltip={'always_visible': False}
+        )])
+        ]
+    return sliders
+
+
+def get_sliders_2(BASE_COSTS):
+    sliders = [
+        html.P(children=dcc.Markdown(
+            "Restrictions on internal movement (**C7**)")),
+        dcc.Slider(
+            id='C7-weight',
+            min=0,
+            max=1,
+            step=0.1,
+            value=BASE_COSTS['C7_Restrictions on internal movement'],
+            marks={0: "0", 0.5: "0.5", 1: "1"},
+            tooltip={'always_visible': False}
+
+        ),
+        html.P(children=dcc.Markdown("International travel controls (**C8**)")),
+        dcc.Slider(
+            id='C8-weight',
+            min=0,
+            max=1,
+            step=0.1,
+            value=BASE_COSTS['C8_International travel controls'],
+            marks={0: "0", 0.5: "0.5", 1: "1"},
+            tooltip={'always_visible': False}
+
+        ),
+        html.P(children=dcc.Markdown("Public information campaigns (**H1**)")),
+        dcc.Slider(
+            id='H1-weight',
+            min=0,
+            max=1,
+            step=0.1,
+            value=BASE_COSTS['H1_Public information campaigns'],
+            marks={0: "0", 0.5: "0.5", 1: "1"},
+            tooltip={'always_visible': False}
+
+        ),
+        html.P(children=dcc.Markdown("Testing policy (**H2**)")),
+        dcc.Slider(
+            id='H2-weight',
+            min=0,
+            max=1,
+            step=0.1,
+            value=BASE_COSTS['H2_Testing policy'],
+            marks={0: "0", 0.5: "0.5", 1: "1"},
+            tooltip={'always_visible': False}
+
+        ),
+        html.P(children=dcc.Markdown("Contact tracing (**H3**)")),
+        dcc.Slider(
+            id='H3-weight',
+            min=0,
+            max=1,
+            step=0.1,
+            value=BASE_COSTS['H3_Contact tracing'],
+            marks={0: "0", 0.5: "0.5", 1: "1"},
+            tooltip={'always_visible': False}
+
+        ),
+        html.P(children=dcc.Markdown("Facial coverings (**H4**)")),
+        dcc.Slider(
+            id='H4-weight',
+            min=0,
+            max=1,
+            step=0.1,
+            value=BASE_COSTS['H6_Facial Coverings'],
+            marks={0: "0", 0.5: "0.5", 1: "1"},
+            tooltip={'always_visible': False}
+        )]
+    return sliders
--- viz_pareto.py
+++ viz_pareto.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 import dash
 import dash_core_components as dcc
+import dash_bootstrap_components as dbc
 import dash_extendable_graph as deg
 import plotly.graph_objects as go
 import dash_html_components as html
@@ -14,11 +15,10 @@ from covid_xprize.nixtamalai.viz_components import get_pareto_data
 from covid_xprize.nixtamalai.viz_components import npi_val_to_cost
 from covid_xprize.nixtamalai.viz_components import get_overall_data
 from covid_xprize.nixtamalai.viz_components import npi_cost_to_val
+from covid_xprize.nixtamalai.viz_components import get_sliders
 import palettable as pltt
 
-
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-TEMPLATE = "ggplot2"
+TEMPLATE = 'plotly_dark'
 
 
 START_DATE = "2020-08-01"
@@ -52,10 +52,6 @@ pareto_data = {"x": pareto[0],
                "name": "Base Prescriptor",
                "showlegend": True,
                }
-# valores de pesos para popular los sliders
-# npis = (weights_df
-#         .drop(columns=['CountryName', 'RegionName'])
-#         .to_dict(orient='records'))[0]
 npis = (weights_df
         .drop(columns=['CountryName', 'RegionName', 'GeoID'])
         .to_dict(orient='records'))[0]
@@ -70,7 +66,7 @@ radar_data = {
 }
 # Gráfica inicial  de predicciones
 predictions = pd.concat(predictions)
-fig_predictions = go.Figure(layout={"title": {"text": "Predictions plot"}, 
+fig_predictions = go.Figure(layout={#"title": {"text": "Predictions plot"}, 
                             "xaxis": {"title": "Date"},
                             "yaxis": {"title": "New Cases per Day"},
                             "template": TEMPLATE
@@ -89,219 +85,106 @@ for idx in predictions.PrescriptionIndex.unique():
         )
     )
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+sliders = get_sliders(BASE_COSTS)
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
 
-app.layout = html.Div(children=[
-    html.H1(children='Visualizing Intervention Plans'),
-
-    # html.Div(children='''
-    #     XPRIZE
-    # '''),
-    html.Div(
-        children =[
-            deg.ExtendableGraph(
-                id='pareto-plot',
-                figure=go.Figure(dict(
-                    data=[pareto_data],
-                    layout={"title": {"text": "Pareto plot"}, 
-                            "xaxis": {"title": "Mean Stringency"},
-                            "yaxis": {"title": "Mean New Cases per Day"},
-                            "legend":{"yanchor": "top","y": 0.99, "x": 0.8},
-                            "template": TEMPLATE
-                            }
-                ))
-            )       
-    ],
-    style={'width': '50%', 'height':'50%', 'display': 'inline-block',"float":"left" }
-    ),
-    html.Div(
-        children =[
-            deg.ExtendableGraph(
-                id='predictions-plot',
-                figure=fig_predictions
-            )       
-    ],
-    style={'width': '50%', 'height':'50%', "float":"left"}
-    ),
-
-    html.Div(
-        children =[
-            dcc.Dropdown(
-                id='model-selector',
-                options=[
-                    {'label': 'Blid Greedy', 'value': 'greedy'},
-                    {'label': 'Nixtamal Surrogate', 'value': 'nixtamal'}
-                ],
-                value='greedy'
-            ),
-            html.Button('Reset', id='reset-val', n_clicks=0),
-            html.P(children=dcc.Markdown("School closing (**C1**)")),
-            dcc.Slider(
-                id='C1-weight',
-                min=0,
-                max=1,
-                step=0.1,
-                value=BASE_COSTS['C1_School closing'],
-                marks = {0:"0", 0.5:"0.5", 1: "1"},
-                tooltip = { 'always_visible': False }
-            ),
-            html.P(children=dcc.Markdown("Workplace closing (**C2**)")),
-            dcc.Slider(
-                id='C2-weight',
-                min=0,
-                max=1,
-                step=0.1,
-                value=BASE_COSTS['C2_Workplace closing'],
-                marks = {0:"0", 0.5:"0.5", 1: "1"},
-                tooltip = { 'always_visible': False }
-
-            ),
-            html.P(children=dcc.Markdown("Cancel public events (**C3**)")),
-            dcc.Slider(
-                id='C3-weight',
-                min=0,
-                max=1,
-                step=0.1,
-                value=BASE_COSTS['C3_Cancel public events'],
-                marks = {0:"0", 0.5:"0.5", 1: "1"},
-                tooltip = { 'always_visible': False }
-
-            ),
-            html.P(children=dcc.Markdown("Restrictions on gathering (**C4**)")),
-            dcc.Slider(
-                id='C4-weight',
-                min=0,
-                max=1,
-                step=0.1,
-                value=BASE_COSTS['C4_Restrictions on gatherings'],
-                marks = {0:"0", 0.5:"0.5", 1: "1"},
-                tooltip = { 'always_visible': False }
-
-            ),
-            html.P(children=dcc.Markdown("Close public transport (**C5**)")),
-            dcc.Slider(
-                id='C5-weight',
-                min=0,
-                max=1,
-                step=0.1,
-                value=BASE_COSTS['C5_Close public transport'],
-                marks = {0:"0", 0.5:"0.5", 1: "1"},
-                tooltip = { 'always_visible': False }
-
-            ),
-            html.P(children=dcc.Markdown("Stay at home requirements (**C6**)")),
-            dcc.Slider(
-                id='C6-weight',
-                min=0,
-                max=1,
-                step=0.1,
-                value=BASE_COSTS['C6_Stay at home requirements'],
-                marks = {0:"0", 0.5:"0.5", 1: "1"},
-                tooltip = { 'always_visible': False }
-
-            ),
-            
+app.layout =html.Div(
+    [
+        dbc.Row(dbc.Col(html.Div(html.H1(children='Visualizing Intervention Plans')))),
+        dbc.Row(html.Hr()),
+        dbc.Row(
+            [
+                dbc.Col(html.Div(sliders[0:3]), width={"size": 2, "offset": 1}),
+                dbc.Col(html.Div(sliders[3:6]), width=2),
+                dbc.Col(html.Div(sliders[6:9]), width=2),
+                dbc.Col(html.Div(sliders[9:12]), width=2),
+                dbc.Col(
+                    [
+                        html.Hr(),
+                        html.Div(dcc.Dropdown(
+                            id='model-selector',
+                            options=[
+                                {'label': 'Blind Greedy', 'value': 'greedy'},
+                                {'label': 'Nixtamal Surrogate', 'value': 'nixtamal'}
+                            ],
+                            value='greedy'
+                        )),
+                        html.Hr(),
+                        html.Div(dcc.DatePickerRange(
+                            id='date-range',
+                            min_date_allowed=date(2020, 8, 1),
+                            max_date_allowed=date(2021, 12, 31),
+                            initial_visible_month=date(2020, 8, 1),
+                            start_date=date(2020, 8, 1),
+                            end_date=date(2020, 9, 1)
+                        ))                       
+                    ],
+                        
+                    width=2),
+                dbc.Col(
+                    [
+                        html.Hr(),
+                        html.Div(dbc.Button('Submit', id='submit-val',color="success",  n_clicks=0)),
+                        html.Hr(),
+                        html.Div(dbc.Button('Reset', id='reset-val', color="warning", n_clicks=0))
+                        
+                    ],
+                        
+                    width=1),
+            ]
+        ),
+        html.Hr(),
+        dbc.Row(
+            [
+                dbc.Col(
+                    html.Div(html.H4("NPI Weights"))
+                    ),               
+                dbc.Col(
+                    html.Div(html.H4("Pareto Plot"))
+                    ),
+                dbc.Col(
+                    html.Div(html.H4("Predictions"))
+                    ),
             ],
-            style={'width': '20%', 'height':'30%','display': 'inline-block',
-                   "background-color": "rgb(237, 237, 237)", "margin-left":"60px"}),
-    html.Div(
-        children=[
-            dcc.DatePickerRange(
-                id='date-range',
-                min_date_allowed=date(2020, 8, 1),
-                max_date_allowed=date(2021, 12, 31),
-                initial_visible_month=date(2020, 8, 1),
-                start_date=date(2020, 8, 1),
-                end_date=date(2020, 9, 1)
-            ),            
-            html.Button('Submit', id='submit-val', n_clicks=0),
-            html.P(children=dcc.Markdown("Restrictions on internal movement (**C7**)")),
-            dcc.Slider(
-                id='C7-weight',
-                min=0,
-                max=1,
-                step=0.1,
-                value=BASE_COSTS['C7_Restrictions on internal movement'],
-                marks = {0:"0", 0.5:"0.5", 1: "1"},
-                tooltip = { 'always_visible': False }
-
-            ),
-            html.P(children=dcc.Markdown("International travel conrols (**C8**)")),
-            dcc.Slider(
-                id='C8-weight',
-                min=0,
-                max=1,
-                step=0.1,
-                value=BASE_COSTS['C8_International travel controls'],
-                marks = {0:"0", 0.5:"0.5", 1: "1"},
-                tooltip = { 'always_visible': False }
-
-            ),
-            html.P(children=dcc.Markdown("Public information campaigns (**H1**)")),
-            dcc.Slider(
-                id='H1-weight',
-                min=0,
-                max=1,
-                step=0.1,
-                value=BASE_COSTS['H1_Public information campaigns'],
-                marks = {0:"0", 0.5:"0.5", 1: "1"},
-                tooltip = { 'always_visible': False }
-
-            ),
-            html.P(children=dcc.Markdown("Testing policy (**H2**)")),
-            dcc.Slider(
-                id='H2-weight',
-                min=0,
-                max=1,
-                step=0.1,
-                value=BASE_COSTS['H2_Testing policy'],
-                marks = {0:"0", 0.5:"0.5", 1: "1"},
-                tooltip = { 'always_visible': False }
-
-            ),
-            html.P(children=dcc.Markdown("Contact tracing (**H3**)")),
-            dcc.Slider(
-                id='H3-weight',
-                min=0,
-                max=1,
-                step=0.1,
-                value=BASE_COSTS['H3_Contact tracing'],
-                marks = {0:"0", 0.5:"0.5", 1: "1"},
-                tooltip = { 'always_visible': False }
-
-            ),
-            html.P(children=dcc.Markdown("Facial coverings (**H4**)")),
-            dcc.Slider(
-                id='H4-weight',
-                min=0,
-                max=1,
-                step=0.1,
-                value=BASE_COSTS['H6_Facial Coverings'],
-                marks = {0:"0", 0.5:"0.5", 1: "1"},
-                tooltip = { 'always_visible': False }
-
-            ),
-    ],
-    style={'width': '20%', 'height':'30%','display': 'inline-block', 'margin-top':0,
-           "background-color": "rgb(237, 237, 237)"}
-    ),
-    # html.Div(
-    #     children=[html.Button('Submit', id='submit-val', n_clicks=0)],
-    #     style={'display': 'inline-block', "float":"right"}
-    # ),
-    html.Div(
-        children=[deg.ExtendableGraph(
-            id='radar-plot',
-            figure=go.Figure(dict(
-                data=[radar_data],
-                layout={"title": {"text": "NPI Weights"},
-                        "template": TEMPLATE}
-            ))
-        )],
-        style={'width': '35%', 'display': 'inline-block', "float": "right"}
-    ),
-])
-
+            align="center",
+        ),
+        dbc.Row(
+            [
+                dbc.Col(
+                    deg.ExtendableGraph(
+                            id='radar-plot',
+                            figure=go.Figure(dict(
+                                data=[radar_data],
+                                layout={"title": {"text": "NPI Weights"},
+                                        "template": TEMPLATE}
+                            ))
+                    ), width=4
+                ),
+                dbc.Col(
+                    deg.ExtendableGraph(
+                        id='pareto-plot',
+                        figure=go.Figure(dict(
+                            data=[pareto_data],
+                            layout={#"title": {"text": "Pareto plot"},
+                                    "xaxis": {"title": "Mean Stringency"},
+                                    "yaxis": {"title": "Mean New Cases per Day"},
+                                    "legend": {"yanchor": "top", "y": 0.99, "x": 0.8},
+                                    "template": TEMPLATE
+                                    }
+                        ))
+                    ), width=4
+                ),
+                dbc.Col(
+                    deg.ExtendableGraph(
+                        id='predictions-plot',
+                        figure=fig_predictions
+                    ), width=4
+                )
+            ],
+            align="center",
+        ),
+    ]
+)
 
 @app.callback([dash.dependencies.Output('pareto-plot', 'extendData'),
                dash.dependencies.Output('predictions-plot', 'extendData')],
@@ -413,5 +296,6 @@ def update_radar_plot(n_clicks, value_c1, value_c2, value_c3, value_c4, value_c5
         }
         return [new_trace, []], []
 
+
 if __name__ == '__main__':
-    app.run_server(debug=True, port=8051)
+    app.run_server(debug=True, port=8050)
